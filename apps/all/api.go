6e5ea06ae79b19311d0cd8ea391908e1652bcc60
296270991@qq.com <296270991@qq.com>
2022-07-13 15:05:14 +0800

    修正污染代码

--- apps/all/api.go
+++
@@ -1,6 +0,0 @@
-package all
-
-import (
-	// 注册所有HTTP服务模块, 暴露给框架HTTP服务器加载
-	_ "github.com/yamakiller/glacier-toolchain/apps/example/api"
-)
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/yamakiller/glacier-toolchain/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
